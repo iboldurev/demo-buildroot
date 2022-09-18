e813a26ee46646b9cfb8d89868333f0ddbb120c3
Ivan Boldyrev <iboldurev@gmail.com>
2022-09-18 20:05:21 +0300

    Update post-build.sh

--- board/pidgc/rpi3/post-build.sh
+++ board/pidgc/rpi3/post-build.sh
@@ -2,7 +2,7 @@
 
 # Copy over binaries
 echo "Optimizing display binary..."
-strip --strip-all "../source/rpi3-display/display"
+#strip --strip-all "../source/rpi3-display/display"
 echo "Copying display binary to image..."
-rm -f "$TARGET_DIR/root/display"
-cp "../source/rpi3-display/display" "$TARGET_DIR/root/display"
+#rm -f "$TARGET_DIR/root/display"
+#cp "../source/rpi3-display/display" "$TARGET_DIR/root/display"
